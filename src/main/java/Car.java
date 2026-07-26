e67cedc4ba3d700a29257364d0340525410565d7
UserShahnoza <oliftaevashahnoza08@gmail.com>
2026-07-27 02:51:04 +0300

    Созданы классы Car и Race

---
+++ src/main/java/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+    String name;
+    int speed;
+
+    public Car(String name, int speed){
+        this.name=name;
+        this.speed=speed;
+
+    }
+    public int getSpeed(){
+        return speed;
+
+    }
+    public String getName(){
+        return name;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Car> listCar = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Введите название машины №" + i);
+            String name = scanner.nextLine();
+
+            while (name.isEmpty()) {
+                System.out.println("Ошибка!");
+                System.out.println("Введите названия машины №" + i);
+                name = scanner.nextLine();
+            }
+            System.out.println("Введите скорость машины №" + i);
+
+            int speed = scanner.nextInt();
+            while (speed <= 0 || speed > 250) {
+                System.out.println("Введите скорость машины №" + i);
+                speed = scanner.nextInt();
+            }
+            scanner.nextLine();
+            Car car = new Car(name, speed);
+            listCar.add(car);
+
+        }
+
+        Race t = new Race();
+        t.race(listCar);
+
+
     }
 }
+
+
