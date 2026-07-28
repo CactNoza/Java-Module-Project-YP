a54ab00e0de066422a1da77335092b80210d79d8
UserShahnoza <oliftaevashahnoza08@gmail.com>
2026-07-28 21:06:21 +0300

    Доработана проверка

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,7 +24,7 @@ public class Main {
                     scanner.next();
                     continue;
                 }
-//                Исправила, хотя в задании указано >0.
+//                Исправила, хотя в задании указано >0
                 speed = scanner.nextInt();
                 if (speed >= 0 && speed <= 250) {
                     scanner.nextLine();
