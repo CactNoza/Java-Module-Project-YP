764bab6c40fb5129a3681b9f8fa49b0a9de6f385
UserShahnoza <oliftaevashahnoza08@gmail.com>
2026-07-28 20:46:20 +0300

    Доработана проверка

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,33 +5,40 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Car> listCar = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+
         for (int i = 1; i <= 3; i++) {
+
             System.out.println("Введите название машины №" + i);
             String name = scanner.nextLine();
 
             while (name.isEmpty()) {
-                System.out.println("Ошибка!");
-                System.out.println("Введите названия машины №" + i);
+                System.out.println("Ошибка! Введите названия машины №" + i);
                 name = scanner.nextLine();
             }
-            System.out.println("Введите скорость машины №" + i);
-
-            int speed = scanner.nextInt();
-            while (speed <= 0 || speed > 250) {
+            int speed;
+            while (true) {
                 System.out.println("Введите скорость машины №" + i);
+
+                if (!scanner.hasNextInt()) {
+                    System.out.println("Ошибка! Введите целое число.");
+                    scanner.next();
+                    continue;
+                }
                 speed = scanner.nextInt();
+                if (speed > 0 && speed <= 250) {
+                    scanner.nextLine();
+                    break;
+                }
+                scanner.nextLine();
+                System.out.println("Ошибка! Скорость должна быть от 1 до 250.");
             }
-            scanner.nextLine();
             Car car = new Car(name, speed);
             listCar.add(car);
-
-        }
-
+            }
         Race t = new Race();
         t.race(listCar);
-
-
+        }
     }
-}
+
 
 
